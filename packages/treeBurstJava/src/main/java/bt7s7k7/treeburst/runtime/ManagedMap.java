d3aeacd2780bbb55b6c2dded47f507e0bed62f10
Branislav Trstenský <bt7s7k7@hotmail.sk>
2025-08-26 16:58:27 +0200

    [Java] Added ManagedMap ctor for Map interface

--- packages/treeBurstJava/src/main/java/bt7s7k7/treeburst/runtime/ManagedMap.java
+++ packages/treeBurstJava/src/main/java/bt7s7k7/treeburst/runtime/ManagedMap.java
@@ -1,6 +1,7 @@
 package bt7s7k7.treeburst.runtime;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import bt7s7k7.treeburst.support.ManagedValue;
 import bt7s7k7.treeburst.support.Primitive;
@@ -10,6 +11,11 @@ public class ManagedMap extends ManagedObject {
 
 	public ManagedMap(ManagedObject prototype) {
 		this(prototype, new HashMap<>());
+	}
+
+	public ManagedMap(ManagedObject prototype, Map<ManagedValue, ManagedValue> entries) {
+		super(prototype);
+		this.entries = new HashMap<>(entries);
 	}
 
 	public ManagedMap(ManagedObject prototype, HashMap<ManagedValue, ManagedValue> entries) {
