287e41d7e9b2989637a5577e1124b217c017b2c1
Branislav Trstenský <bt7s7k7@hotmail.sk>
2025-09-19 19:40:29 +0200

    [Java] Table.new() from entries

--- packages/treeBurstJava/src/main/java/bt7s7k7/treeburst/standard/TableApi.java
+++ packages/treeBurstJava/src/main/java/bt7s7k7/treeburst/standard/TableApi.java
@@ -1,10 +1,14 @@
 package bt7s7k7.treeburst.standard;
 
 import static bt7s7k7.treeburst.runtime.ExpressionEvaluator.findProperty;
+import static bt7s7k7.treeburst.support.ManagedValueUtils.ensureArgumentTypes;
+import static bt7s7k7.treeburst.support.ManagedValueUtils.ensureString;
 
 import java.util.List;
 
 import bt7s7k7.treeburst.runtime.GlobalScope;
+import bt7s7k7.treeburst.runtime.ManagedArray;
+import bt7s7k7.treeburst.runtime.ManagedMap;
 import bt7s7k7.treeburst.runtime.ManagedObject;
 import bt7s7k7.treeburst.runtime.ManagedTable;
 import bt7s7k7.treeburst.runtime.NativeFunction;
@@ -22,7 +26,7 @@ public class TableApi extends LazyTable {
 	protected void initialize() {
 		this.declareProperty("prototype", this.globalScope.TablePrototype);
 
-		this.declareProperty("new", NativeFunction.simple(this.globalScope, List.of("this"), (args, scope, result) -> {
+		this.declareProperty("new", NativeFunction.simple(this.globalScope, List.of("this", "properties?"), (args, scope, result) -> {
 			// @summary: Creates a new object that inherits from this table's `prototype` property, assuming it exists. Otherwise an exception is generated.
 			var self = args.get(0);
 
@@ -37,7 +41,53 @@ public class TableApi extends LazyTable {
 				return;
 			}
 
-			result.value = new ManagedTable(prototype_1);
+			var table = new ManagedTable(prototype_1);
+
+			if (args.size() == 2) {
+				if (args.get(1) instanceof ManagedArray entries) {
+					int index = -1;
+					for (var element : entries.elements) {
+						index++;
+
+						if (!(element instanceof ManagedArray kv) || kv.elements.size() > 2) {
+							result.setException(new Diagnostic("Entry at index " + index + " is not a pair", Position.INTRINSIC));
+							return;
+						}
+
+						// In this case, either the key or value was set to void, so don't add this entry
+						if (kv.elements.size() < 2) continue;
+
+						var key = ensureString(kv.elements.get(0), scope, result).value;
+						if (result.label != null) return;
+
+						var value = kv.elements.get(1);
+
+						// If the property already exists overwrite it
+						if (!table.declareProperty(key, value)) {
+							table.setOwnProperty(key, value);
+						}
+					}
+				} else {
+					args = ensureArgumentTypes(args, List.of("this", "properties"), List.of(ManagedTable.class, ManagedMap.class), scope, result);
+					if (result.label != null) return;
+
+					var entries = args.get(1).getMapValue();
+
+					for (var kv : entries.entries.entrySet()) {
+						var key = ensureString(kv.getKey(), scope, result).value;
+						if (result.label != null) return;
+
+						var value = kv.getValue();
+
+						// If the property already exists overwrite it
+						if (!table.declareProperty(key, value)) {
+							table.setOwnProperty(key, value);
+						}
+					}
+				}
+			}
+
+			result.value = table;
 		}));
 	}
 
